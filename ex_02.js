4fd7f802638e97f321a2e0875ba893437d430bc6
sarah1reindl <sarah1.reindl@epitech.digital>
2024-10-08 10:20:45 +0200

    ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,11 @@
+let currentRotation = 0;
+
+function rotateCircle(step) {
+    const circle = document.getElementById('circle');
+
+    currentRotation += step;
+
+    circle.style.transform = `rotate(${currentRotation}deg)`;
+}
+document.querySelectorAll(".rotate-button").forEach(button =>
+button.addEventListener("click", ()=> rotateCircle(parseInt(button.value))))
